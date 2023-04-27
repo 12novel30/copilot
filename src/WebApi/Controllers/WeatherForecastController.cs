aafd3f7a33edca099ca5e8dc4bcc4db1621e9501
12novel30 <79260472+12novel30@users.noreply.github.com>
2023-04-27 03:19:20 +0000

    add a run api project

--- src/WebApi/Controllers/WeatherForecastController.cs
+++ src/WebApi/Controllers/WeatherForecastController.cs
@@ -29,6 +29,19 @@ public class WeatherForecastController : ControllerBase
         })
         .ToArray();
     }
+
+    // add a new action method that returns a list of WeatherForecast objects and accepts a parameter of DateRange type with the [FromQuery] attribute
+    [HttpGet("range", Name = "GetWeatherForecastRange")]
+    public IEnumerable<WeatherForecast> Get([FromQuery] DateRange range)
+    {
+        return Enumerable.Range(1, range.Length).Select(index => new WeatherForecast
+        {
+            Date = DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
+            TemperatureC = Random.Shared.Next(-20, 55),
+            Summary = Summaries[Random.Shared.Next(Summaries.Length)]
+        })
+        .ToArray();
+    }
 }
 
 // add a new class of DateRange that contains a Length property of integer type
@@ -37,15 +50,4 @@ public class DateRange
     public int Length { get; set; }
 }
 
-// add a new action method that returns a list of WeatherForecast objects and accepts a parameter of DateRange type with the [FromQuery] attribute
-[HttpGet("range", Name = "GetWeatherForecastRange")]
-public IEnumerable<WeatherForecast> Get([FromQuery] DateRange range)
-{
-    return Enumerable.Range(1, range.Length).Select(index => new WeatherForecast
-    {
-        Date = DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
-        TemperatureC = Random.Shared.Next(-20, 55),
-        Summary = Summaries[Random.Shared.Next(Summaries.Length)]
-    })
-    .ToArray();
-}
+
